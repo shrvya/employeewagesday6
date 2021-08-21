8f147778f6dc8a9e63659a07da4a946366892b28
shrvya <shrivyashetty@gmail.com>
2021-08-21 22:15:37 +0530

    branch added

--- employeewages.java
+++ employeewages.java
@@ -8,20 +8,24 @@ public class employeewages {
        final int IS_FULL_TIME=1;
        final int IS_PART_TIME=2;
        final int WAGE_PER_HOUR=20;
+       final int WDAYS_PER_MONTH=20;
        int emphr=0;
        int empwage=0;
        int empcheck=(int) (Math.floor(Math.random()*10)%3);
        switch(empcheck)
        {
        case IS_FULL_TIME:emphr=16;
+                        System.out.println("FULL TIME EMP");
                          break;
        case IS_PART_TIME:emphr=8;
+       System.out.println("PART TIME EMP");
                          break;
        default:emphr=0;
+       System.out.println(" EMP ABSENT");
               break;
        }
-       empwage=emphr*WAGE_PER_HOUR;
-       System.out.println("employee wage is "+empwage);
+       empwage=emphr*WAGE_PER_HOUR*WDAYS_PER_MONTH;
+       System.out.println("monthly employee wage is "+empwage);
 	}
 
 }
