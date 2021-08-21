ed4a935067bcefaa378803cf5d090cb38370f83e
shrvya <shrivyashetty@gmail.com>
2021-08-21 21:46:26 +0530

    branch added

--- employeewages.java
+++ employeewages.java
@@ -4,7 +4,16 @@ public class employeewages {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println("welcome to employee wage computation");
+       final int IS_FULL_TIME=1;
+       double empcheck=Math.floor(Math.random()*10)%2;
+       if(empcheck==IS_FULL_TIME)
+       {
+    	   System.out.println("Employee is present");
+       }
+       else {
+    	   System.out.println("Employee is absent");
+       }
 	}
 
 }
