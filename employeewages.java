b606ee51ee45ae0638428505d297035f2d1d77e2
shrvya <shrivyashetty@gmail.com>
2021-08-25 16:22:03 +0530

    branch added

--- employeewages.java
+++ employeewages.java
@@ -2,31 +2,44 @@ package employeewages;
 
 public class employeewages {
  
+    private int numofcompany=0;
 	public static final int IS_FULL_TIME=1;
 	public static final int  IS_PART_TIME=2;
+	private CompanyEmpWage[] CompanyEmpWageArray;
+	//constructor to initialize the members of the class
+	public employeewages()
+	{
+		CompanyEmpWageArray=new CompanyEmpWage[5];
+	}
 	
-	private final String company;
-	private final int WAGE_PER_HOUR;
-	public final int WDAYS_PER_MONTH;
-	public final int MAX_WRK_HRSMONTH;
-    public employeewages(String company,int WAGE_PER_HOUR,int WDAYS_PER_MONTH,int MAX_WRK_HRSMONTH )
-    {
-    	this.company=company;
-    	this. WAGE_PER_HOUR= WAGE_PER_HOUR;
-    	this. WDAYS_PER_MONTH= WDAYS_PER_MONTH;
-    	this.MAX_WRK_HRSMONTH=MAX_WRK_HRSMONTH;
-    }
-    
-    public boolean computewage()
+	
+	 public void addCompanyEmpWage(String company ,int WAGE_PER_HOUR,int WDAYS_PER_MONTH,int MAX_WRK_HRSMONTH )
+	 {
+		 CompanyEmpWageArray[numofcompany]=new CompanyEmpWage(company,WAGE_PER_HOUR, WDAYS_PER_MONTH,MAX_WRK_HRSMONTH );
+		 numofcompany++;
+	 }
+	 
+	 private void computewage()
+	 {
+		 for(int i=0;i<numofcompany;i++)
+		 {
+			 
+			 CompanyEmpWageArray[i].setTotalWage(this.computewage(CompanyEmpWageArray[i]));
+		 }
+	 }
+	 //method to compute wage for  different companies
+    public int computewage(CompanyEmpWage companyEmpWage)
     {
     	 int emphr=0;
-         int empwage=0;
+        
+    	 //int empwage=0;
         int totalwhrs=0;
         int totalwdays=0;
+        
         int totalempwage=0;
         
         
-        while(totalwhrs<MAX_WRK_HRSMONTH && totalwdays<WDAYS_PER_MONTH)
+        while(totalwhrs<=companyEmpWage.MAX_WRK_HRSMONTH && totalwdays<companyEmpWage.WDAYS_PER_MONTH)
         {
       	  int empcheck=(int) (Math.floor(Math.random()*10)%3);
             switch(empcheck)
@@ -43,16 +56,12 @@ public class employeewages {
             }
             totalwhrs+=emphr;
             totalwdays++;
-            empwage=emphr*WAGE_PER_HOUR;
-            totalempwage+=empwage;
-            
+            totalempwage=totalwhrs*companyEmpWage.WAGE_PER_HOUR;
+           
+            System.out.println(" total hrs is "+totalwhrs);
+            System.out.println("total days is "+totalwdays);
         }
-        
-         System.out.println(" employee wage is "+empwage);
-         System.out.println("total employee wage is "+empwage);
-         System.out.println(" total hrs is "+totalwhrs);
-         System.out.println("total days is "+totalwdays);
-         return false ;
+        return totalempwage;       
   	}
 
     
@@ -60,12 +69,11 @@ public class employeewages {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("welcome to employee wage computation");
-        employeewages dmart=new  employeewages("dmart" ,20,20,100);
-        employeewages kmart=new  employeewages("kmart" ,10,20,150);
-       dmart.computewage();
-      
-       kmart.computewage();
-       
+        employeewages wmpwages=new employeewages();
+        wmpwages.addCompanyEmpWage("dmart" ,20,20,100);
+        wmpwages.addCompanyEmpWage("kmart" ,10,20,150);
+        wmpwages.computewage();
+        wmpwages.toString();
 	}
 }
 
